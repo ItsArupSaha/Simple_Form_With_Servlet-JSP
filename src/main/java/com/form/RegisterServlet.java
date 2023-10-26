39cc7e28220611b969f85b3f58012a24e5d03faf
ItsArupSaha <sahaarup911@gmail.com>
2023-10-27 00:51:55 +0600

    Simple Form with Servlet&JST

---
+++ src/main/java/com/form/RegisterServlet.java
@@ -0,0 +1,37 @@
+package com.form;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.setContentType("Text/html");
+		PrintWriter outPrintWriter = resp.getWriter();
+		outPrintWriter.println("Welcome to Register Servlet");
+		
+		String name = req.getParameter("user_name");
+		String password = req.getParameter("user_password");
+		String email = req.getParameter("user_email");
+		String gender = req.getParameter("user_gender");
+		String course = req.getParameter("user_course");
+		String cond = req.getParameter("condition");
+		
+		if(cond != null) {
+			outPrintWriter.println("<h2>Name: "+name+"</h2>");
+			outPrintWriter.println("<h2>Password: "+password+"</h2>");
+			outPrintWriter.println("<h2>Email: "+email+"</h2>");
+			outPrintWriter.println("<h2>Gender: "+gender+"</h2>");
+			outPrintWriter.println("<h2>Course: "+course+"</h2>");
+		}else {
+			outPrintWriter.println("<h2>Not Accepted</h2>");
+		}
+		
+	}
+}
